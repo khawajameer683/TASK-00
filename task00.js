b4606714c608cf689eee964a784f8571722f7cb0
khawaja meer Afridi <khawajameerafridi683@gmail.com>
2024-02-24 11:36:54 +0500

    the task00

---
+++ task00.js
@@ -0,0 +1,38 @@
+// SUM OF NUMBER
+// let number = 456;
+// let digits = number.toString().split('');
+// let sum = 0;
+
+// for (let digit of digits) {
+//     sum += parseInt(digit);
+// }
+// console.log("Sum of digits of", number, "is", sum);
+
+
+// SUM OF RANG OF TWO NUMBER
+// let num1 = 2;
+// let num2 = 5;
+// let sum = 0;
+
+// for (let i = num1; i <= num2; i++) {
+//     sum += i;
+// }
+// console.log("Sum of the range from", num1, "to", num2, "is", sum);
+
+
+// // AVERAGE OF GIVEN NUMBER
+// var numbers = [1, 2, 3, 4, 5];
+// var sum = 0;
+// for (var i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// var average = sum / numbers.length;
+// console.log("Average:", average);
+
+
+
+// SWAPPED THE FIRST AND LAST CHARACTER
+// var str = "name";
+// var swappedStr = str.charAt(str.length - 1) + str.substring(1, str.length - 1) + str.charAt(0);
+// console.log("Original String:", str);
+// console.log("Swapped String:", swappedStr);
